ed890a886fe62305fb5ac45fc3f0d00006a4b9bf
Akumarjava <firebullaman@gmail.com>
2022-08-17 07:44:39 +0530

    This is the project status after the addition of the certain files that were added in the later phase.

---
+++ main.py
@@ -0,0 +1,5 @@
+#This is the main script for the python
+import teacher as t
+sk = ["Numpy","Pandas","Geography","Seaborn"]
+t1 = t.teacher("Ankit Raj",sk)
+t1.show()
---
+++ teacher.py
@@ -0,0 +1,19 @@
+#This is the implementation of the python object oriented approach for the teacher
+if __name__ == "__main__":
+    print("running the file as the main")
+else:
+    class teacher:
+        def __init__(self,name,skills):
+            self.name = name
+            self.skills = skills
+        def count_skills(self):
+            count=0
+            for i in self.skills:
+                count+=1
+            return count
+        def show(self):
+            print(f"My name is {self.name}")
+            print("My skills are:")
+            for skill in self.skills:
+                print(skill,end=" ")
+            print("\nTotal no of skills {}".format(self.count_skills()))
